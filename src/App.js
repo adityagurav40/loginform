6b5d82a5ee4917ecc481eb7a5937f4985357355c
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-06-17 23:56:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./Components/Form";
+
+
+const App = ()=>{
+
+
+    return(
+        <div className="app">
+            <h1>SignUp</h1>
+            <Form/>
+        </div>
+    )
+}
+
+export default App;
